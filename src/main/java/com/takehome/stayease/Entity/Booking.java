5ac688f341e301270de45a730752f5c785bb3295
Soumya7008069898 <signups7423@gmail.com>
2025-06-24 17:39:06 +0530

    ME_BUILDOUT_STAYEASE_MODULE_L1  (Crio.Do)

--- src/main/java/com/takehome/stayease/Entity/Booking.java
+++ src/main/java/com/takehome/stayease/Entity/Booking.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+// uihchjvnfn
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
